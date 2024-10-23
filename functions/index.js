a8108585b012f2fcb69a1b6bfd74e65b6d4c9396
ali yasser <ali.yasser@imperialstud.io>
2024-10-23 06:27:19 +0300

    index.js modified

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,13 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const createBlog = require('./modules/blog/create-blog-post');
+const deleteBlog = require('./modules/blog/delete-blog-post');
+const updateBlog = require('./modules/blog/update-blog-post');
+const getBlogs = require('./modules/blog/get-blog-posts');
+const getBlog = require('./modules/blog/get-blog-post');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+module.exports = {
+    createBlog,
+    deleteBlog,
+    updateBlog,
+    getBlogs,
+    getBlog,
+};
